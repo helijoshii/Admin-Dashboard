2937208d900e674ee2b12ab9b0d08368f07fa9d9
helijoshii <helijoshi45@gmail.com>
2023-04-08 21:35:11 +0000

    sdfghjk

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
@@ -8,21 +7,20 @@ import './App.css';
 const App = () => {
   return (
     <div>
-        <BrowserRouter>
-        <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-          <TooltipComponent
-              content="Settings"
-              position="Top"
-            >
+      <BrowserRouter>
+        <div className='flex rlative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style = {{zIndex: '1000'}}>
+            <TooltipComponent content="Settings" position="Top">
               <button>
                 <FiSettings />
               </button>
 
             </TooltipComponent>
+
           </div>
-          </div>
-        </BrowserRouter>
+
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
