343bc8c67a6a03a9ccf0f8d9c531abb0e044ff44
helijoshii <helijoshi45@gmail.com>
2023-04-10 03:56:25 +0000

    dfghjk

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM  from 'react-dom';
 import './index.css';
 import App from './App';
-import {createRoot} from 'react-dom/client';
-
-
  
 
 ReactDOM.render(<App /> , document.getElementById('root'));
